e487f1aa34b370f4c5de1d3280cd67122183531f
shaodahong <darryshaw@gmail.com>
2021-02-27 15:46:30 +0800

    use nextra

---
+++ theme.config.js
@@ -0,0 +1,23 @@
+export default {
+  repository: 'https://github.com/shaodahong/h5-interview', // project repo
+  docsRepository: 'https://github.com/shaodahong/h5-interview', // docs repo
+  branch: 'master', // branch of docs
+  path: '/', // path of docs
+  titleSuffix: ' / h5-interview',
+  nextLinks: true,
+  prevLinks: true,
+  search: true,
+  customSearch: null, // customizable, you can use algolia for example
+  darkMode: true,
+  footer: true,
+  footerText: 'MIT 2020 © shaodahong.',
+  footerEditOnGitHubLink: true, // will link to the docs repo
+  logo: 'H5-Interview',
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Nextra: the next docs builder" />
+      <meta name="og:title" content="Nextra: the next docs builder" />
+    </>
+  ),
+}
