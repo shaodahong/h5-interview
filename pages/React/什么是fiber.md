7b4bb42a9a995d7ff41f97f6bcf410222014c1ff
shaodahong <darryshaw@gmail.com>
2021-02-27 19:57:02 +0800

    增加事件流

---
+++ pages/React/什么是fiber.md
@@ -0,0 +1,5 @@
+# 什么是 React Fiber？
+
+## 参考资料
+
+- [React Fiber Architecture](https://github.com/acdlite/react-fiber-architecture)
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import 'nextra-theme-docs/style.css'
 
 export default function Nextra({ Component, pageProps }) {
+  console.log(pageProps);
   return <Component {...pageProps} />
 }
+
+
