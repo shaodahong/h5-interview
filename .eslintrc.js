3b08ffd281ddbb601e5f54cea46729710a989aa5
shaodahong <darryshaw@gmail.com>
2021-03-23 14:12:30 +0800

    fix markdown lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,13 @@
 module.exports = {
+  root: true,
   extends: [
     'airbnb',
     'airbnb/hooks',
     'plugin:prettier/recommended',
     'prettier/react',
-    'plugin:markdown/recommended',
     'plugin:mdx/recommended',
   ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', 'mdx'] }],
+  },
 }
